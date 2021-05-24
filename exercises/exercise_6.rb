fe3a1a8f8db33aa522619a474c760dfb5fa70568
Omar Irfan <omar.irfan18@gmail.com>
2021-05-24 20:01:06 +0000

    excercise 7

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -9,3 +9,10 @@ puts "Exercise 6"
 puts "----------"
 
 # Your code goes here ...
+puts Store.has_many(:employees)
+puts Store.belongs_to(:store)
+@store1.employees.create(first_name: "Omar", last_name: "Irfan", hourly_rate: 60)
+@store1.employees.create(first_name: "McSteve", last_name: "Timmy", hourly_rate: 45)
+@store2.employees.create(first_name: "Piccolo", last_name: "Jr", hourly_rate: 45)
+@store2.employees.create(first_name: "Son", last_name: "Gohan", hourly_rate: 75)
+@store1.employees.create(first_name: "Son", last_name: "Goku", hourly_rate: 95)
--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -10,3 +10,29 @@ puts "Exercise 7"
 puts "----------"
 
 # Your code goes here ...
+class Employee
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :hourly_rate, numericality: { only_integer: true, greater_than: 40, less_than: 200}
+  validates :store, presence: true
+
+end
+
+class Store
+  validates :name, length: { minimum: 3 }
+  validates :annual_revenue, numericality: { only_integer: true, minimum: 0}
+  validate :check_has_apparel
+  
+  def check_has_apparel 
+    mens_apparel || womens_apparel 
+  end
+
+end
+
+puts "Enter a store name:"
+print "> "
+store_name = $stdin.gets.chomp
+
+new_store = Store.create(name: store_name)
+
+puts new_store.errors.full_messages
